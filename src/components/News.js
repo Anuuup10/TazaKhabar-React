b012a46643a8ed60dccb8401b9978dba9ed87406
Anup Chaudhary <74030059+Anup10430@users.noreply.github.com>
2026-07-11 09:07:44 +0545

    Feat: Added Pagination

--- src/components/News.js
+++ src/components/News.js
@@ -7,17 +7,44 @@ export class News extends Component {
     super();
     this.state = {
         articles: [],
-        loading: false
+        loading: false,
+        page: 1
+
     }
 
   }
 
   async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=c7d24296200441ec9cfb25d2488ede28";
+    let url = "https://newsapi.org/v2/everything?q=apple&from=2026-07-10&to=2026-07-10&sortBy=popularity&apiKey=c7d24296200441ec9cfb25d2488ede28&page=1&pageSize=20";
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({articles: parsedData.articles})
+    this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
   }
+  
+  handlePrevClick = async () => {
+      let url = `https://newsapi.org/v2/everything?q=apple&from=2026-07-10&to=2026-07-10&sortBy=popularity&apiKey=c7d24296200441ec9cfb25d2488ede28&page=${this.state.page - 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        page: this.state.page - 1,
+        articles: parsedData.articles
+      })
+  }
+
+  handleNextClick = async () => {
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+    }else{
+      let url = `https://newsapi.org/v2/everything?q=apple&from=2026-07-10&to=2026-07-10&sortBy=popularity&apiKey=c7d24296200441ec9cfb25d2488ede28&page=${this.state.page + 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles
+      })
+    } 
+  }
+
   render() {
     return (
       <div className='container my-3'>
@@ -27,10 +54,14 @@ export class News extends Component {
           return(
             <div className="col-md-4" key={element.url}>
               <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
-            </div>
-            )
-          })} 
-          </div>
+              </div>
+              )
+             })} 
+             </div>
+             <div className="container d-flex justify-content-between">
+              <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+              <button disabled={this.state.page >= 4} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+             </div>
       </div>
     )
   }
