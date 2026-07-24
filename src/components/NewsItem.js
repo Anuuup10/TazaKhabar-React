0fa1b7a105ae17b20854941be3186c7682759bc9
Anup Chaudhary <74030059+Anup10430@users.noreply.github.com>
2026-07-24 10:20:07 +0545

    Feat: Infinite Scroll

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date } = this.props;
     return (
-      <div className='my-3'>
+      <div className="my-3">
         <div className="card">
-          <img src={!imageUrl?"https://images.mktw.net/im-01535996/social":imageUrl} className="card-img-top" alt="..."/>
+          <img
+            src={
+              !imageUrl
+                ? "https://images.mktw.net/im-01535996/social"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                BY {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
           </div>
-       </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
